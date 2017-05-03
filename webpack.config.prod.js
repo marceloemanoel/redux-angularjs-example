c1b090923a25e579779c49e88d5ca6a49ad9110b
Marcelo Emanoel B. Diniz <marceloemanoel@gmail.com>
2017-05-03 15:02:30 -0300

    Enable Tree-Shaking algorithm for production builds

---
+++ webpack.config.prod.js
@@ -0,0 +1,12 @@
+const devConfig = require("./webpack.config");
+const webpack = require("webpack");
+const BabiliPlugin = require("babili-webpack-plugin");
+
+module.exports = Object.assign({}, devConfig, {
+    plugins: devConfig.plugins.concat([
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new BabiliPlugin()
+    ])
+});
