17b399fa0b4e052a59558d2781cdf54e9e4a2f01
Marcelo Emanoel B. Diniz <marceloemanoel@gmail.com>
2017-05-02 21:10:56 -0300

    Move files from src/main/reddit-angular to src/main/

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,5 @@
 import * as angular from "angular";
-import AppModule from "./reddit-angular/AppModule";
+import AppModule from "./AppModule";
 
 angular.element(document).ready(() => {
   angular.bootstrap(document.querySelector("app"), [AppModule]);
